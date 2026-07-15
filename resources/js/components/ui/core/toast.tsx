1b1c7cbbcd85bed18369e77638e640b5bf6d38d7
libravelabs <libravelabs@gmail.com>
2026-07-15 12:45:03 +0700

    latest depedencies update

--- resources/js/components/ui/core/toast.tsx
+++ resources/js/components/ui/core/toast.tsx
@@ -1,27 +1,27 @@
-import { Toaster as ToasterPrimitive, type ToasterProps } from 'sonner';
-import { useTheme } from 'next-themes';
+import { Toaster as ToasterPrimitive, type ToasterProps } from "sonner";
+import { useTheme } from "next-themes";
 
 function Toast({ ...props }: ToasterProps) {
-    const { theme = 'system' } = useTheme();
+  const { theme = "system" } = useTheme();
 
-    return (
-        <ToasterPrimitive
-            theme={theme as ToasterProps['theme']}
-            className="toaster group"
-            toastOptions={{
-                className:
-                    '*:data-icon:self-start font-sans has-data-description:*:data-icon:mt-1 *:data-icon:mt-0.5 backdrop-blur-2xl',
-            }}
-            style={
-                {
-                    '--normal-bg': 'var(--popover)',
-                    '--normal-text': 'var(--popover-foreground)',
-                    '--normal-border': 'var(--border)',
-                } as React.CSSProperties
-            }
-            {...props}
-        />
-    );
+  return (
+    <ToasterPrimitive
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      toastOptions={{
+        className:
+          "*:data-icon:self-start font-sans has-data-description:*:data-icon:mt-1 *:data-icon:mt-0.5 backdrop-blur-2xl",
+      }}
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  );
 }
 
 export type { ToasterProps };
